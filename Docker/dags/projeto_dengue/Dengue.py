d9cba90fe24bf85d5fc799653f7fe3c10e7864cd
Lucas-Sobreira <lucas-andrade2011@live.com>
2023-06-11 16:03:17 -0300

    'final_version'

--- Docker/dags/projeto_dengue/Dengue.py
+++ Docker/dags/projeto_dengue/Dengue.py
@@ -9,10 +9,7 @@ from airflow.hooks.S3_hook import S3Hook
 
 import sys
 sys.path.insert(0, '/usr/local/airflow/dags/projeto_dengue')
-# from projeto_dengue.coleta import coleta_dados, df_dengue, df_municipios, url
-from projeto_dengue.saving_raw_old import coleta_dados, df_dengue, df_municipios, url, n_week
-# from projeto_dengue.saving_raw_old import save, path
-
+from projeto_dengue.saving_raw import coleta_dados, df_dengue, df_municipios, url, n_week
 
 default_args = {
     'owner': 'grupo_fia',
@@ -35,35 +32,26 @@ start = DummyOperator(
     task_id='start',
     dag=dag)
 
-python_task_01 = PythonOperator(
+python_api = PythonOperator(
                 task_id='coleta_dados', 
                 python_callable=coleta_dados, 
                 op_kwargs={'df_dengue': df_dengue, 'df_municipios': df_municipios, 'url': url, 'n_week': n_week},
                 dag=dag)
 
-# python_task_02 = PythonOperator(
-#                 task_id='save_Minio', 
-#                 python_callable=save, 
-#                 op_kwargs={'path': path},
-#                 dag=dag)
-
-def teste_arquivo():
+def dados_dengue():
     # Upload generated file to Minio
     s3 = S3Hook('minio_s3')
     s3.load_file("/usr/local/airflow/dags/projeto_dengue/df_dengue.csv",
                     key=f"df_dengue.csv",
                     bucket_name="raw")
 
-salvando = PythonOperator(
+salvando_dengue = PythonOperator(
                 task_id='teste_arquivo', 
-                python_callable=teste_arquivo,
+                python_callable=dados_dengue,
                 dag=dag)
 
 finish = DummyOperator(
     task_id='finish',
     dag=dag)
 
-
-# start >> python_task_01 >> python_task_02 >> finish
-
-start >> python_task_01 >> salvando >> finish
+start >> python_api >> salvando_dengue >> finish
